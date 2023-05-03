757929900830796b21731ce7dd2a41389eddfd14
dustisme <androsrivas@outlook.com>
2023-05-03 16:04:39 +0200

    methods created

--- app/models/jsonPersistence.php
+++ app/models/jsonPersistence.php
@@ -1,29 +1,40 @@
 <?php
+include '../app/models/classes/task.php';
 
 class jsonPersistence implements jsonPersistenceActions {  
     
     private Array $task_array = array();
 
-
     function __construct(){
-
+        if(file_exists(dirname(__DIR__).'/../web/json/data.json')){
+            $this->task_array = json_decode(file_get_contents(dirname(__DIR__).'/../web/json/data.json'), false);
+          }
     }
-    public function listTasks() {
+    function listTasks(): Array {
         return $this->task_array;
     }
-    public function viewTask() {
+    function viewTask($task_id) {
+        return $this->task_array[$this->searchTask($task_id)];
+    }
+    function editOneTask($task_id) {
 
     }
-    public function editOneTask() {
-
+    function addTask() {}
+    function deleteTask($task_id) {
+        foreach($this->task_array as $selected_task) {
+            if($selected_task->getId() == $task_id) {
+                unset($selected_task);
+            } else {
+                echo "Task not found";
+            }
+        }
     }
-    public function addTask() {
-
-    }
-    public function deleteTask() {
-
-    }
-    public function searchTask() {
-        
+    function searchTask($task_id) {
+        foreach($this->task_array as $selected_task) {
+            if($selected_task->getId() == $task_id) {
+                return $selected_task;
+            }
+        }
+        return dirname(__DIR__).'/../app/views/scripts/error/error.phtml';
     }
 }
