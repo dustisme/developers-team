3a3c5962444d64a556707e0f54c0ef1a3e1911ce
dustisme <androsrivas@outlook.com>
2023-05-09 10:41:59 +0200

    controller functions created (not finished)

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -6,5 +6,34 @@
  */
 class ApplicationController extends Controller 
 {
+    private $persistence;
+    public function __construct() {
+        $this->persistence = new Persistence();
+    }
+	public function indexAction() {
+		$this->view->listTasks = $this->persistence->listTasks();
+	}
+
+    public function viewTaskAction() {
+        $this->view->oneTask = $this->persistence->viewTask($_POST[]);
+    }
+
+    public function editOneTaskAction() {
+        
+    }
+
+    public function addTaskAction() {
+
+    }
+    public function deleteTaskAction() {
+        $this->view->deteleTask = $this->persistence->deleteTask($_POST[]);
+    }
+    public function searchTaskAction() {
+
+    }
 	
+	public function checkAction()
+	{
+		echo "hello from test::check";
+	}
 }
